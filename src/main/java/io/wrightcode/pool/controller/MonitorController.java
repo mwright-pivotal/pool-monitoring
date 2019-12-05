e8326fe07536d87c732acb343bc115cdf44ed66e
mwright <mwright@pivotal.io>
2019-12-05 09:13:23 -0500

    custom metrics with actuator

--- src/main/java/io/wrightcode/pool/controller/MonitorController.java
+++ src/main/java/io/wrightcode/pool/controller/MonitorController.java
@@ -1,11 +1,17 @@
 package io.wrightcode.pool.controller;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.wrightcode.pool.model.Status;
 
 @RestController("MonitorController")
@@ -14,8 +20,15 @@ public class MonitorController {
 
 	Logger log = Logger.getLogger(MonitorController.class.getName());
 	
+	@Autowired
+	MeterRegistry meterRegistry;
+	
 	@RequestMapping(value="/status", method = RequestMethod.POST)
-	public void sendStatus(Status stat) {
+	public void sendStatus(@RequestBody Status stat) {
+		stat.setTimeUpdated(new Date());
 		log.info("Received update: " + stat);
+		Gauge
+	      .builder("pool.phValue", () -> Long.parseLong(stat.getPhValue()) )
+	      .register(meterRegistry);
 	}
 }
--- src/main/java/io/wrightcode/pool/model/Status.java
+++ src/main/java/io/wrightcode/pool/model/Status.java
@@ -1,8 +1,50 @@
 package io.wrightcode.pool.model;
 
-public class Status {
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Status implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	String monitorUUID = "";
 	String textStatus = "";
+	String phValue = "";
+	String tdsValue = "";
+	public String getTdsValue() {
+		return tdsValue;
+	}
+
+	public void setTdsValue(String tdsValue) {
+		this.tdsValue = tdsValue;
+	}
+	Date timeUpdated;
+	
+	public Status(String monitorUUID, String phValue, String orpValue) {
+		this.monitorUUID = monitorUUID;
+		this.phValue = phValue;
+		this.timeUpdated = new Date();
+	}
+	
+	public Date getTimeUpdated() {
+		return timeUpdated;
+	}
+	public void setTimeUpdated(Date timeUpdated) {
+		this.timeUpdated = timeUpdated;
+	}
+	public String getPhValue() {
+		return phValue;
+	}
+	public void setPhValue(String phValue) {
+		this.phValue = phValue;
+	}
+	public String getOrpValue() {
+		return orpValue;
+	}
+	public void setOrpValue(String orpValue) {
+		this.orpValue = orpValue;
+	}
+	String orpValue = "";
 	public String getMonitorUUID() {
 		return monitorUUID;
 	}
@@ -15,4 +57,7 @@ public class Status {
 	public void setTextStatus(String textStatus) {
 		this.textStatus = textStatus;
 	}
+	public String toString() {
+		return (new Timestamp(timeUpdated.getTime())) + " : " + monitorUUID + ": " + phValue + " : " + orpValue + " : " + tdsValue;
+ 	}
 }
