65bcd7e603740184286c258ff3b1a43e831da28d
michaelwright <michaelwright@192.168.0.75>
2019-12-14 15:51:19 -0500

    piepline fixes

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,7 +7,7 @@ M2_HOME="${HOME}/.m2"
 M2_CACHE="${ROOT_FOLDER}/maven"
 [[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 
-cd venues
+cd pool-monitor
 mvn package
-cp target/venue-0.0.1-SNAPSHOT.jar ../build-output/
+cp target/PoolMonitor-0.0.1-SNAPSHOT.jar ../build-output/
 cp manifest.yml ../build-output/
