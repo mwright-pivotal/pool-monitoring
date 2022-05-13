4b8471051cf438a4788dcf55138ad9a6a527b4b6
Mike Wright <wrightmi@vmware.com>
2022-05-13 18:05:58 +0100

    update dependencies

--- src/test/java/io/wrightcode/pool/PoolMonitorApplicationTests.java
+++ src/test/java/io/wrightcode/pool/PoolMonitorApplicationTests.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
